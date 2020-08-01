ee49881125b382292eb5d5ea1240df5c35243fbd
MTSS <blackiviagic@gmail.com>
2020-08-01 09:01:06 -0400

    started building model

---
+++ model_build.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 08:58:48 2020
+
+@author: Mario
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+
+df = pd.read_csv('bmw_cleaned.csv')
